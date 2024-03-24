806268f1d46577315487fb7cb518842eb678eb98
EMeremnu Edison Maduabuchi <edisonemeremnu@gmail.comm>
2024-03-24 21:24:57 +0100

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,11 @@
 #Puppet to make changes to our configuration file
 file_line { 'ssh_conf':
-        path  => '/etc/ssh/ssh_config',
-        line  => 'PasswordAuthentication no',
+	ensure => 'present',
+        path   => '/etc/ssh/ssh_config',
+        line   => 'PasswordAuthentication no',
 }
 file_line { 'ssh_conf':
-        path  => '/etc/ssh/ssh_config',
-        line  => 'IdentityFile ~/.ssh/school',
+        ensure => 'present',
+        path   => '/etc/ssh/ssh_config',
+        line   => 'IdentityFile ~/.ssh/school',
 }
